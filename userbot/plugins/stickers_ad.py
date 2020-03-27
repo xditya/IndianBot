a509f088f21d4b21a58a0154e84c55952ebf3b80
indianbhaiya <61813271+indianbhaiya@users.noreply.github.com>
2020-03-27 13:12:16 +0530

    Update stickers_ad.py

--- userbot/plugins/stickers_ad.py
+++ userbot/plugins/stickers_ad.py
@@ -44,8 +44,8 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"JayukeAdultStickers"
-    packshortname = f"Jayu_ke_adult_stickers"  # format: Uni_Borg_userid
+    packname = f"IndianBhai™kestickerrs"
+    packshortname = f"IndianBhai™_ke_stickers"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@UniBorg_Sticker.png"
@@ -54,11 +54,11 @@ async def _(event):
     if is_a_s:
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
-        packname = f"JayukeAnimated"
-        if userid == 719877937:
-            packshortname = "Jayu_Adult_Animated"
+        packname = f"Indian™keAnimated"
+        if userid == 953414679:
+            packshortname = "Indian™_Animated"
         else:
-            packshortname = f"Jayu_Adult_nimated" # format: Uni_Borg_userid
+            packshortname = f"IndianBhai™_Animated" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
